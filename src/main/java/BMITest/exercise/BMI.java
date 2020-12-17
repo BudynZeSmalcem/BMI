bf8dba6d8fc4b68e858bff21f229d04ef6ee6232
BudynZeSmalcem <pawek47@gmail.com>
2020-12-17 09:49:11 +0100

    zrobione zadania

--- src/main/java/BMITest/exercise/BMI.java
+++ src/main/java/BMITest/exercise/BMI.java
@@ -8,7 +8,18 @@ public class BMI {
     // weight or growth < 0? - "I don't think so"
 
     public String checkBmi(int weight, double growth) {
-        String result = "";
-        return result;
+        double BMI = weight / Math.pow(growth, 2);
+        if (weight < 0 || growth < 0) {
+            return "I don't think so";
+        } else if (weight == 0 || growth == 0) {
+            return "do not divide by 0";
+        }
+        if (BMI < 18.5) {
+            return "get some weight";
+        } else if (BMI >= 25) {
+            return "you need to grow";
+        } else {
+            return "good weight";
+        }
     }
 }
